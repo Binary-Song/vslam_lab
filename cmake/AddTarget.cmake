fb8b1e501b2750d4ba47623fa6ea0c91aedf5e81
binary-song <yeh189@126.com>
2021-05-12 20:43:57 +0800

    update cmake files

--- cmake/AddTarget.cmake
+++ cmake/AddTarget.cmake
@@ -1,18 +1,17 @@
 include(CMakeParseArguments)
-#[[ example:
-
+#[[   
 
 AddTarget(
-    exe       有`exe`选项表示要生成可执行文件，没有表示要生成静态库 
-    name      目标名
-    src       源文件1 源文件2 ...
-    includes  包含路径1 包含路径2 ... 
-    libs      链接目标1 链接目标2 ...
-    depends   依赖模块1 依赖模块2 ...
+    [exe]       
+    <name      目标名>
+    [src       源文件1 源文件2 ...]
+    [includes  包含路径1 包含路径2 ...] 
+    [libs      链接目标1 链接目标2 ...]
+    [depends   依赖模块1 依赖模块2 ...]
 )
 
-依赖模块 X 表示增加包含路径 X_INCLUDE_DIRS，并增加链接目标 X_LIBRARIES。
-现在如果X_INCLUDE_DIRS为空，则自动调用find_package(X)找。
+创建一个目标，`exe`选项表示要生成可执行文件，没有该选项表示要生成静态库。
+依赖模块 X 表示在目标的include目录里面加上 X_INCLUDE_DIRS，链接库里面加上 X_LIBRARIES。
 
 ]] 
 function(AddTarget)
@@ -23,6 +22,9 @@ function(AddTarget)
         "libs;includes;src;depends"  # multi value args
         ${ARGN}
     )
+    if(arg_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "unrecognized argument(s): ${arg_UNPARSED_ARGUMENTS}")
+    endif()
 
     if(arg_exe)
         add_executable("${arg_name}" ${arg_src})
@@ -36,22 +38,24 @@ function(AddTarget)
     foreach(_dep   ${arg_depends})
 
         if(NOT ${_dep}_INCLUDE_DIRS )
-            message("package `${_dep}` seems yet to be found, trying find_package .. ")
+            message(FATAL_ERROR "package `${_dep}` does not seem to exist")
             find_package(${_dep} REQUIRED)
         endif() 
 
-        list(APPEND Includes "${${_dep}_INCLUDE_DIRS}") 
-        message("${${_dep}_LIBRARIES}")
-        list(APPEND Libs     "${${_dep}_LIBRARIES}")
+        if(${_dep}_INCLUDE_DIRS)
+            list(APPEND Includes "${${_dep}_INCLUDE_DIRS}")
+        endif()
+ 
+        if(${_dep}_LIBRARIES) 
+            list(APPEND Libs     "${${_dep}_LIBRARIES}")
+        endif()
         
-    endforeach(_dep arg_depends)
+    endforeach()
 
-    message("i=${Includes}")
-    message("l=${Libs}")
+  #  message("i=${Includes}")
+  #  message("l=${Libs}")
 
     target_include_directories("${arg_name}" PUBLIC ${Includes})
     target_link_libraries("${arg_name}" PUBLIC ${Libs})
-
-
-
+    
 endfunction(AddTarget) 
